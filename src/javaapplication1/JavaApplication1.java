28064dae53590f3347bf3db1933ed1d5fc2b28b5
Camilo Rodriguez <crodriguez@usbbog.edu.co>
2013-08-06 08:30:29 -0500

    inicial

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author 201
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        JOptionPane.showMessageDialog(null, "hola");
+    }
+}
